b66d785fc1ca5fff12435fe047cbfded03ec04e1
Frostleaft07 <zx7unknow@gmail.com>
2024-08-17 00:25:31 +0000

    lcm: nt36525b_txd_psc_hdp_dsi_vdo_lcm: Switch to update_tpfw_notifier_call_chain for firmware updates during reset
    - Changed firmware update function from lcd_queue_load_tp_fw to update_tpfw_notifier_call_chain after SET_RESET_PIN.
    - Ensures correct firmware update flow during device reset.
    
    Signed-off-by: Frostleaft07 <zx7unknow@gmail.com>

--- drivers/misc/mediatek/lcm/nt36525b_txd_psc_hdp_dsi_vdo_lcm/nt36525b_txd_psc_hdp_dsi_vdo_lcm.c
+++ drivers/misc/mediatek/lcm/nt36525b_txd_psc_hdp_dsi_vdo_lcm/nt36525b_txd_psc_hdp_dsi_vdo_lcm.c
@@ -559,7 +559,7 @@ static void lcm_init(void)
 	MDELAY(10);
 	SET_RESET_PIN(1);
 	MDELAY(15);
-	lcd_queue_load_tp_fw();
+	update_tpfw_notifier_call_chain(1,NULL);
 	if (lcm_dsi_mode == CMD_MODE) {
 		push_table(NULL, init_setting_cmd, sizeof(init_setting_cmd) / sizeof(struct LCM_setting_table), 1);
 		pr_debug("nt36525b_txd_lcm_mode = cmd mode :%d----\n", lcm_dsi_mode);
